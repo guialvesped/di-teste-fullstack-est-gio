a3628fd8eb258783d0cefc98f9d67a12230ddee7
Guilherme Alves Pedroso <alvespedrosog@gmail.com>
2024-10-05 20:13:29 -0300

    Adding cents and the plan name
    I added the way that we could get the plans names and the cents if in the future we alter the prices

--- src/main.js
+++ src/main.js
@@ -18,7 +18,8 @@ async function main() {
 
 async function getLowestPrice(ctx) {
   const { message } = ctx.update;
-  const url = message.text;
+  const linkRegex = /https:\/\/www\.divulgadorinteligente\.com\//i;
+  const url = message.text.match(linkRegex);
   let lowestPlanPrice = 0;
   let lowestPlanName = ""; 
 
@@ -28,8 +29,14 @@ async function getLowestPrice(ctx) {
 
     const $ = cheerio.load(response.data);
     const prices = [];
+    const planNames = [];
 
-    $('.currency').each((element) => {
+    $('.head').each((i,element) => {
+      const name = $(element).text().trim();
+      planNames.push(name);
+    })
+
+    $('.currency').each((i,   element) => {
       const priceText = $(element).text().trim();
       const price = parseFloat(priceText);
       if (price > 0) {
@@ -37,16 +44,27 @@ async function getLowestPrice(ctx) {
       }
     });
 
+    $('.cent').each((i,   element) => {
+      if(!isNaN(element)){
+        const centText = $(element).text().trim();
+        const cent = parseFloat(centText);
+        prices[i] =+ (cent/100)
+      }
+    });
+
     if (prices.length > 0) {
       lowestPlanPrice = Math.min(...prices);
+      const lowestPriceIndex = prices.findIndex(price => price === lowestPlanPrice);
+      lowestPlanName = planNames[lowestPriceIndex]
       await ctx.reply(
-        `O plano pago mais barato é o Plano X e custa "${lowestPlanPrice}"`
+        `O plano pago mais barato é o "${lowestPlanName}" e custa R$${lowestPlanPrice}`
       );
     } else {
       await ctx.reply("Nenhum preço encontrado");
     }
 
   } catch (error) {
+    console.error("Erro ao buscar os dados:", error);
     await ctx.reply("Ocorreu um erro ao buscar os dados.");
   }
 }
